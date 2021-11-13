fc8e06d4f553b79016c5c9b4e9225dcc67da20a4
Claudia Sofía Asanza Romero <clasrome@espol.edu.ec>
2021-11-13 13:09:32 -0500

    Distribucion componentes

--- golanglexer.py
+++ golanglexer.py
@@ -1,1 +1,24 @@
+import ply.lex as lex
+"""
 
+EMANUEL
+Tokens,
+Palabras reservadas, 
+Caracteres (),
+
+CLAUDIA
+Tipos de datos,
+asignacion,
+formateo %s, %d,
+
+ISSAC
+Definir variables, 
+Operadores,
+salto de  linea,
+
+
+
+errores,
+
+
+"""
